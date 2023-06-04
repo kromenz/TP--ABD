9e3477a6c3478d43fe99338c210e0ee1a8293f0c
I Fero I <106333313+IFero04@users.noreply.github.com>
2023-06-04 22:04:39 +0100

    Delete Comandos directory

--- Comandos/MelhorVoltaCorrida.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.nome AS NomePiloto, e.valor AS TempoVoltaMaisRapido
-FROM Piloto p
-INNER JOIN Estatisticas e ON p.id_piloto = e.id_piloto
-WHERE e.tipo_stat = 6
-  AND e.id_corrida = 1 
-ORDER BY e.valor ASC;
--- Comandos/VW_MelhorTempoGPChina.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW VW_MelhorTempoGPChina AS
-SELECT c."data", MIN(e.valor) AS MelhorTempo
-FROM Corrida c
-INNER JOIN Estatisticas e ON c.id_corrida = e.id_corrida
-WHERE c.nome = 'GP China' AND e.tipo_stat = 1
-GROUP BY c."data";
